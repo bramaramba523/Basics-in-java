171489dd958dbeef80c4086111bcc0fe6e670bcb
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:28:54 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,16 @@
+package NewPack;
+
+public class count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int count=10;
+while(count>0)
+{
+	System.out.println(count);
+	count++;
+}
+System.out.println("Blast-off");
+	}
+
+}
