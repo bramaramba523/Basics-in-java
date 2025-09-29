3e6ed14b08e687b0f2ba64b6e9309cca5750c41d
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:15:56 +0530

    Add files via upload

---
+++ ForPrint.java
@@ -0,0 +1,17 @@
+package pack12;
+
+public class ForPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+		
+			System.out.println("Hello");
+
+		}
+		
+	}
+
+}
