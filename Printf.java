af3e1907cdbf598f4818a04aac713983f59a1d49
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:17:35 +0530

    Add files via upload

---
+++ Printf.java
@@ -0,0 +1,13 @@
+package pack12;
+
+public class Printf{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//formated output
+		String name="Brama";
+		int i=6;
+		System.out.printf("Name: %s,I: %d%n",name,i);
+	}
+
+}
