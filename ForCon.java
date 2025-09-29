2adee3c70fcb2fdee7058f15079eddcd55a68492
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:15:31 +0530

    Add files via upload

---
+++ ForCon.java
@@ -0,0 +1,16 @@
+package pack12;
+
+public class ForCon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// for loop with conditional statement(break,continue,jump,goto)
+		for(int i=1; i<=10;i++)
+{
+	if(i==5) continue;
+	if(i==8) break;
+	System.out.println(i);
+}
+	}
+
+}
