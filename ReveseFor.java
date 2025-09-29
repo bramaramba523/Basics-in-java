ce4280b982494298f07955f310e901c10fb27960
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:18:07 +0530

    Add files via upload

---
+++ ReveseFor.java
@@ -0,0 +1,18 @@
+package pack12;
+
+public class ReveseFor {
+
+	public static void main(String[] args) {
+
+		for(int i=100; i>=50;i-=4)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
+// write a program to the number between 100-50
+//%4
+//%2
+//%2&4
+// right angled triangle in opposite direction
