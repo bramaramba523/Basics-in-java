a8e609901dfae0715d3381beeb45c05c44825c95
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:15:06 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package pack12;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1,fact=1,num=4;
+while(i<=num) {
+	fact=fact*1;
+	i++;
+	                              
+}
+System.out.println("Factorial"+fact);
+	}
+
+}
+//write a program that performs simple calcultor using methods add sub div mul?
+//to check the number is palindrom using a methods?string/number?
+// to print  the number is dvisible by 12 based on the user input through method skeleton parameter?
+// factorial using amethod?
