322d59a751a4becd0390f17076af2baa7ba1406b
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 23:16:37 +0530

    Add files via upload

---
+++ MultipleFor.java
@@ -0,0 +1,22 @@
+package pack12;
+
+public class MultipleFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//for(int i=0, j=10; i<j;i++, j--)
+//{
+//System.out.println();	}
+int rows=5;
+for(int i=0;i<=rows;i++)
+{
+for(int j=1;j<=i;j++)
+{
+	System.out.print("*");
+
+}
+System.out.println();
+}
+}
+}
+	
